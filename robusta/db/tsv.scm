2bda03ddb11d6be835d6fe3ee5a35b870c78ea18
krzysckh <krzysckh@wp.pl>
2024-04-07 15:26:32 +0200

    https://gitlab.com/owl-lisp/owl/-/issues/41

--- robusta/db/tsv.scm
+++ robusta/db/tsv.scm
@@ -1,5 +1,8 @@
 #| doc
-tsv thingy
+inneficient tsv dbms.
+
+schema for ``
+
 |#
 (define-library
   (robusta db tsv)
@@ -16,18 +19,21 @@ tsv thingy
    close
    get-all
    get-column
+   filter-by
+   delete-from
    )
 
   (begin
     (define (open-f fname schema)
       (let* ((exists (sys/file? fname))
-             (fo (open-append-file fname))
+             (fa (open-append-file fname))
              (fi (open-input-file fname)))
         (when (not exists)
           (sys/chmod fname #o777 0))
         (list
+         `(F . ,fname)
          `(fi . ,fi)
-         `(fo . ,fo)
+         `(fa . ,fa)
          `(check-schema
            . ,(λ (lst)
                 (and
@@ -38,7 +44,9 @@ tsv thingy
                    (λ (n)
                      (let ((check-f (cadr (list-ref schema n)))
                            (thing (list-ref lst n)))
-                       (check-f thing)))
+                       (if (check-f thing)
+                           #t
+                           (error "(robusta db tsv) schema-error: " thing))))
                    (iota 0 1 (length schema)))))))
          `(serialize
            . ,(λ (lst)
@@ -48,46 +56,62 @@ tsv thingy
                                     ((list-ref (list-ref schema n) 2) (list-ref lst n)))
                                   (iota 0 1 (length lst))))))
                   (substring s 0 (- (string-length s) 1)))))
+         `(deserialize
+           . ,(λ (lst)
+                (map
+                 (λ (n) ((list-ref (list-ref schema n) 3) (list-ref lst n)))
+                 (iota 0 1 (length lst)))))
+
          `(columns . ,(map car schema))
          )))
 
     (define (getvs th)
       (let ((chk (cdr (assq 'check-schema th)))
             (fi (cdr (assq 'fi th)))
-            (fo (cdr (assq 'fo th)))
+            (fa (cdr (assq 'fa th)))
+            (F (cdr (assq 'F th)))
             (serialize (cdr (assq 'serialize th)))
+            (deserialize (cdr (assq 'deserialize th)))
             (columns (cdr (assq 'columns th))))
-        (values chk fi fo serialize columns)))
+        (values chk fi fa F serialize deserialize columns)))
+
+    (define (create-schema-str cols)
+      (foldr (λ (acc s) (string-append acc "\t" s))
+             ""
+             (map (λ (t) (if (string? t) t (symbol->string t)))
+                  cols)))
 
     (define (create fname schema)
-      (lets ((ret (open-f fname schema))
-             (c fi fo s cs (getvs ret))
-             (schema-str (foldr (λ (acc s) (string-append acc "\t" s))
-                                ""
-                                (map (λ (t) (if (string? t) t (symbol->string t)))
-                                     (map car schema)))))
-        (print-to fo schema-str)
-        ret))
+      (when (sys/file? fname)
+        (error "(robusta db tsv) file already exists, don't (create) - (open) " fname))
+      (let ((ret (open-f fname schema)))
+        (lets ((c fi fa F s ds cs (getvs ret))
+               (schema-str (create-schema-str cs)))
+          (print-to fa schema-str)
+          ret)))
 
     (define (open fname schema)
       (open-f fname schema))
 
     (define (insert-into th lst)
-      (lets ((chk fi fo serialize columns (getvs th)))
-       (if (chk lst)
-           (print-to fo (serialize lst))
-           (error "(robusta db tsv): " "schema-error"))))
+      (lets ((chk fi fa F serialize ds columns (getvs th)))
+       (when (chk lst)
+         (print-to fa (serialize lst)))))
 
     (define (close th)
-      (lets ((c fi fo s cs (getvs th)))
+      (lets ((c fi fa F s ds cs (getvs th)))
         (close-port fi)
-        (close-port fo)))
+        (close-port fa)))
 
     (define cuttab (string->regex "c/\t/"))
     ;; th → ll
     (define (get-all th)
-      (lets ((chk fi fo serialize cs (getvs th)))
-        (lmap cuttab (lcdr (lines fi)))))
+      (lets ((chk fi fa F serialize ds cs (getvs th)))
+        ;; (sys/seek-set fi 0)
+        ;; (print (force-ll (lines fi)))
+        (sys/seek-set fi 0)
+        ;; (lmap (λ (s) (ds (cuttab s))) (lcdr (lines fi)))))
+        (lmap (λ (s) (ds (cuttab s))) (lcdr (byte-stream->lines (file->byte-stream fi))))))
 
     (define (get-coln columns colsym acc)
       (cond
@@ -98,9 +122,23 @@ tsv thingy
 
     ;; th sym → ll;
     (define (get-column th colsym)
-      (lets ((chk fi fo serialize columns (getvs th))
+      (lets ((chk fi fa F serialize ds columns (getvs th))
              (n (get-coln columns colsym 0)))
         (if n
-            (lmap (λ (v) (print v) (list-ref v n)) (get-all th))
+            (lmap (λ (v) (list-ref v n)) (get-all th))
             (error "(robusta db tsv)" "no-such-column"))))
+
+    ;; TODO: apply ma implementation-restriction dla dużych list iirc
+    (define (filter-by th pred)
+      (lets ((chk fi fa F serialize ds columns (getvs th)))
+        (lkeep (λ (l) (apply pred l)) (get-all th))))
+
+    (define (delete-from th pred)
+      (lets ((chk fi fa F serialize ds columns (getvs th)))
+        (let ((kept (force-ll (lremove (λ (l) (apply pred l)) (get-all th)))))
+          ;; (sys/close
+           (sys/open F (sys/O_TRUNC) #o777)
+           ;; )
+          (print-to fa (create-schema-str columns))
+          (map (λ (v) (print v) (insert-into th v)) kept))))
     ))
